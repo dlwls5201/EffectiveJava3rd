39c4881adb5fc47f608932ab518995f583bef011
leejinseong <dlwls5201@naver.com>
2019-04-16 00:32:20 +0900

    add item03

--- src/chapter02/item03/SingleTon1.java
+++ src/chapter02/item03/SingleTon1.java
@@ -1,16 +1,17 @@
 package chapter02.item03;
 
+//public 이나 protected 생성자가 없으므로 SingleTon 클래스가 초기화 될 때 만들어진 인스턴스가 전체 시스템에서 하나뿐임이 보장된다.
 public class SingleTon1 {
 
-    private static final SingleTon1 INSTANCE = new SingleTon1();
+    public static final SingleTon1 INSTANCE = new SingleTon1();
 
-    //public 이나 protected 생성자가 없으므로 SingleTon1 클래스가 초기화 될 때
-    //만들어진 인스턴스가 전체 시스템에서 하나뿐임이 보장된다.
+    private int count = 0;
+
     private SingleTon1() {
-
+        //리플렉션 방어 코드
+        count++;
+        if(count != 1) {
+            throw new IllegalStateException("this object should be singletone");
+        }
     }
-
-    public static SingleTon1 getInstance() { return INSTANCE; }
-
-    public void leaveTheBuilding() {}
 }
--- src/chapter02/item03/SingleTon2.java
+++ src/chapter02/item03/SingleTon2.java
@@ -1,11 +1,23 @@
 package chapter02.item03;
 
-public enum SingleTon2 {
+import java.util.function.Supplier;
 
-    INSTANCE;
+//public 이나 protected 생성자가 없으므로 SingleTon 클래스가 초기화 될 때 만들어진 인스턴스가 전체 시스템에서 하나뿐임이 보장된다.
+public class SingleTon2 {
 
-    //enum 또한 프로퍼티 및 메서드를 가질 수 있다.
-    public String getName() {
-        return "BlackJin";
+    private static final transient SingleTon2 INSTANCE = new SingleTon2();
+
+    private SingleTon2() {
+
+    }
+
+    public static SingleTon2 getInstance() {
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+
+        //getInstance 에 대한 레퍼런스를 Supplier 타입으로 만들 수 있다.
+        Supplier<SingleTon2> s2Supplier = SingleTon2::getInstance;
     }
 }
